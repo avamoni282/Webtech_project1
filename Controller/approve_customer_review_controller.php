0cb9d3b7ae1cdfda6854a6ebea4ec4c62f23ec5c
avamoni282 <146236443+avamoni282@users.noreply.github.com>
2024-05-20 21:16:47 +0600

    Delete Controller directory

--- Controller/approve_customer_review_controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    require_once('../Model/customer_review_model.php'); 
-    
-    $id = $_GET['id'];
-    
-     if(approveReview($id)) header('location:../View/approve_customer_review.php');
-
-?>
